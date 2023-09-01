75cc5e4d56375319b0f71da693ce877c42936537
Jon Monarrez <135302142+j0n-m@users.noreply.github.com>
2023-08-31 23:17:14 -0500

    Add error handler to api response and display more weather data to webpage

--- main.js
+++ main.js
@@ -91,7 +91,8 @@ ___CSS_LOADER_EXPORT___.push([module.id, `:root {
 body {
   font-family: sans-serif, system-ui;
   font-size: 1.2rem;
-  background: linear-gradient(#a8ffff, #ceffff);
+  /* background: linear-gradient(#a8ffff, #ceffff); */
+  background-color: #ebebeb;
   min-height: 100svh;
 }
 
@@ -108,6 +109,7 @@ body {
   justify-content: space-between;
   align-items: center;
   padding: .5rem 1rem;
+  box-shadow: 1px 1px 2px black;
 }
 
 .leftHeader {
@@ -158,22 +160,101 @@ form {
 }
 
 
-/* Content */
+/* ADDED */
+.content-container {
+  width: 60%;
+  margin: 0 auto;
+}
 
 .content {
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-auto-flow: row;
-  /* justify-content: center; */
-  border: 1px solid red;
-  padding: 1rem 2rem;
+  padding: 1rem;
+  /* background-color: lightgray; */
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 }
 
 .card {
-  border: 1px solid blue;
-  border-radius: var(--default-border-radius);
+  border: 1px solid gray;
+  display: flex;
+  background-color: white;
+  flex-direction: column;
   padding: 1rem;
-}`, "",{"version":3,"sources":["webpack://./src/style.css"],"names":[],"mappings":"AAEA;EACE,8BAA8B;EAC9B,oCAAoC;EACpC,6BAA6B;AAC/B;;AAEA;;;EAGE,SAAS;EACT,UAAU;EACV,sBAAsB;AACxB;;AAEA;EACE,kCAAkC;EAClC,iBAAiB;EACjB,6CAA6C;EAC7C,kBAAkB;AACpB;;AAEA;EACE,YAAY;AACd;;AAEA,WAAW;;AAEX;EACE,wBAAwB;EACxB,aAAa;EACb,0CAA0C;EAC1C,8BAA8B;EAC9B,mBAAmB;EACnB,mBAAmB;AACrB;;AAEA;EACE,YAAY;EACZ,0BAA0B;EAC1B,2CAA2C;EAC3C,cAAc;EACd,kEAAkE;AACpE;;AAEA;;GAEG;;AAEH;EACE,aAAa;EACb,WAAW;AACb;;AAEA;EACE,kBAAkB;AACpB;;;AAGA,YAAY;;AAEZ;EACE,yBAAyB;EACzB,mBAAmB;EACnB,cAAc;EACd,YAAY;EACZ,eAAe;EACf,yBAAyB;AAC3B;;AAEA;EACE,oCAAoC;AACtC;;AAEA;EACE,kBAAkB;EAClB,wBAAwB;EACxB,gBAAgB;EAChB,2BAA2B;EAC3B,kBAAkB;EAClB,UAAU;EACV,aAAa;AACf;;;AAGA,YAAY;;AAEZ;EACE,aAAa;EACb,0BAA0B;EAC1B,mBAAmB;EACnB,6BAA6B;EAC7B,qBAAqB;EACrB,kBAAkB;AACpB;;AAEA;EACE,sBAAsB;EACtB,2CAA2C;EAC3C,aAAa;AACf","sourcesContent":["@import url('default.css');\n\n:root {\n  --header-main-color: lightblue;\n  --textbox-outline-active-color: blue;\n  --default-border-radius: 10px;\n}\n\n*,\n*::before,\n*::after {\n  margin: 0;\n  padding: 0;\n  box-sizing: border-box;\n}\n\nbody {\n  font-family: sans-serif, system-ui;\n  font-size: 1.2rem;\n  background: linear-gradient(#a8ffff, #ceffff);\n  min-height: 100svh;\n}\n\n.container {\n  height: 100%;\n}\n\n/* HEADER */\n\n.header {\n  /* position: relative; */\n  display: flex;\n  background-color: var(--header-main-color);\n  justify-content: space-between;\n  align-items: center;\n  padding: .5rem 1rem;\n}\n\n.leftHeader {\n  color: white;\n  border: 1.5px solid orange;\n  border-radius: var(--default-border-radius);\n  padding: .5rem;\n  background: linear-gradient(rgb(255, 216, 143), rgb(255, 199, 96));\n}\n\n/* .middleHeader {\n  display: flex;\n} */\n\nform {\n  display: flex;\n  gap: 0 1rem;\n}\n\n.rightHeader {\n  position: relative;\n}\n\n\n/* BUTTONS */\n\n.smallBtn {\n  border: 1.5px solid white;\n  border-radius: 10px;\n  padding: .5rem;\n  color: white;\n  cursor: pointer;\n  background-color: inherit;\n}\n\n.smallBtn:hover {\n  background-color: rgb(195, 239, 253);\n}\n\n.headerSettings {\n  position: absolute;\n  border: 1.5px solid gray;\n  border-top: none;\n  background-color: lightgray;\n  border-radius: 2px;\n  right: 4px;\n  padding: 1rem;\n}\n\n\n/* Content */\n\n.content {\n  display: grid;\n  grid-template-columns: 1fr;\n  grid-auto-flow: row;\n  /* justify-content: center; */\n  border: 1px solid red;\n  padding: 1rem 2rem;\n}\n\n.card {\n  border: 1px solid blue;\n  border-radius: var(--default-border-radius);\n  padding: 1rem;\n}"],"sourceRoot":""}]);
+  border-radius: 10px;
+  min-width: 400px;
+}
+
+.cardHeader {
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid gray;
+  padding-bottom: 8px;
+}
+
+.cardWeatherConditions {
+  display: flex;
+  padding: 8px 0;
+  gap: 1rem;
+}
+
+.cardWeatherImportant {
+  display: flex;
+  align-items: center;
+  gap: .5rem;
+
+}
+
+.cardWeatherConditionsLeft,
+.cardWeatherConditionsRight {
+  flex: 1;
+}
+
+.rc {
+  width: 50%;
+}
+
+.current-real-feel-div,
+.current-wind-div {
+  display: flex;
+  justify-content: space-between;
+}
+
+.current-weather-logo {
+  width: 62px;
+  height: 62px;
+  /* background-color: blue; */
+}
+
+.cardWeatherDetails {
+  display: flex;
+  gap: 1rem;
+}
+
+.cardWeatherDetailsLeft,
+.cardWeatherDetailsRight {
+  flex: 1;
+}
+
+.cardWeatherDetailsLeft>div,
+.cardWeatherDetailsRight>div {
+  display: flex;
+  border-bottom: 1px solid gray;
+  padding: 8px 0;
+}
+
+.cardWeatherDetailsLeft>div>p,
+.cardWeatherDetailsRight>div>p {
+  flex: 1;
+}
+
+.cardWeatherDetailsLeft>div>p:nth-child(2),
+.cardWeatherDetailsRight>div>p:nth-child(2) {
+  text-align: right;
+}
+
+.current-temp-label {
+  font-size: 1.75rem;
+}`, "",{"version":3,"sources":["webpack://./src/style.css"],"names":[],"mappings":"AAEA;EACE,8BAA8B;EAC9B,oCAAoC;EACpC,6BAA6B;AAC/B;;AAEA;;;EAGE,SAAS;EACT,UAAU;EACV,sBAAsB;AACxB;;AAEA;EACE,kCAAkC;EAClC,iBAAiB;EACjB,mDAAmD;EACnD,yBAAyB;EACzB,kBAAkB;AACpB;;AAEA;EACE,YAAY;AACd;;AAEA,WAAW;;AAEX;EACE,wBAAwB;EACxB,aAAa;EACb,0CAA0C;EAC1C,8BAA8B;EAC9B,mBAAmB;EACnB,mBAAmB;EACnB,6BAA6B;AAC/B;;AAEA;EACE,YAAY;EACZ,0BAA0B;EAC1B,2CAA2C;EAC3C,cAAc;EACd,kEAAkE;AACpE;;AAEA;;GAEG;;AAEH;EACE,aAAa;EACb,WAAW;AACb;;AAEA;EACE,kBAAkB;AACpB;;;AAGA,YAAY;;AAEZ;EACE,yBAAyB;EACzB,mBAAmB;EACnB,cAAc;EACd,YAAY;EACZ,eAAe;EACf,yBAAyB;AAC3B;;AAEA;EACE,oCAAoC;AACtC;;AAEA;EACE,kBAAkB;EAClB,wBAAwB;EACxB,gBAAgB;EAChB,2BAA2B;EAC3B,kBAAkB;EAClB,UAAU;EACV,aAAa;AACf;;;AAGA,UAAU;AACV;EACE,UAAU;EACV,cAAc;AAChB;;AAEA;EACE,aAAa;EACb,iCAAiC;EACjC,aAAa;EACb,sBAAsB;EACtB,SAAS;AACX;;AAEA;EACE,sBAAsB;EACtB,aAAa;EACb,uBAAuB;EACvB,sBAAsB;EACtB,aAAa;EACb,mBAAmB;EACnB,gBAAgB;AAClB;;AAEA;EACE,aAAa;EACb,8BAA8B;EAC9B,6BAA6B;EAC7B,mBAAmB;AACrB;;AAEA;EACE,aAAa;EACb,cAAc;EACd,SAAS;AACX;;AAEA;EACE,aAAa;EACb,mBAAmB;EACnB,UAAU;;AAEZ;;AAEA;;EAEE,OAAO;AACT;;AAEA;EACE,UAAU;AACZ;;AAEA;;EAEE,aAAa;EACb,8BAA8B;AAChC;;AAEA;EACE,WAAW;EACX,YAAY;EACZ,4BAA4B;AAC9B;;AAEA;EACE,aAAa;EACb,SAAS;AACX;;AAEA;;EAEE,OAAO;AACT;;AAEA;;EAEE,aAAa;EACb,6BAA6B;EAC7B,cAAc;AAChB;;AAEA;;EAEE,OAAO;AACT;;AAEA;;EAEE,iBAAiB;AACnB;;AAEA;EACE,kBAAkB;AACpB","sourcesContent":["@import url('default.css');\n\n:root {\n  --header-main-color: lightblue;\n  --textbox-outline-active-color: blue;\n  --default-border-radius: 10px;\n}\n\n*,\n*::before,\n*::after {\n  margin: 0;\n  padding: 0;\n  box-sizing: border-box;\n}\n\nbody {\n  font-family: sans-serif, system-ui;\n  font-size: 1.2rem;\n  /* background: linear-gradient(#a8ffff, #ceffff); */\n  background-color: #ebebeb;\n  min-height: 100svh;\n}\n\n.container {\n  height: 100%;\n}\n\n/* HEADER */\n\n.header {\n  /* position: relative; */\n  display: flex;\n  background-color: var(--header-main-color);\n  justify-content: space-between;\n  align-items: center;\n  padding: .5rem 1rem;\n  box-shadow: 1px 1px 2px black;\n}\n\n.leftHeader {\n  color: white;\n  border: 1.5px solid orange;\n  border-radius: var(--default-border-radius);\n  padding: .5rem;\n  background: linear-gradient(rgb(255, 216, 143), rgb(255, 199, 96));\n}\n\n/* .middleHeader {\n  display: flex;\n} */\n\nform {\n  display: flex;\n  gap: 0 1rem;\n}\n\n.rightHeader {\n  position: relative;\n}\n\n\n/* BUTTONS */\n\n.smallBtn {\n  border: 1.5px solid white;\n  border-radius: 10px;\n  padding: .5rem;\n  color: white;\n  cursor: pointer;\n  background-color: inherit;\n}\n\n.smallBtn:hover {\n  background-color: rgb(195, 239, 253);\n}\n\n.headerSettings {\n  position: absolute;\n  border: 1.5px solid gray;\n  border-top: none;\n  background-color: lightgray;\n  border-radius: 2px;\n  right: 4px;\n  padding: 1rem;\n}\n\n\n/* ADDED */\n.content-container {\n  width: 60%;\n  margin: 0 auto;\n}\n\n.content {\n  padding: 1rem;\n  /* background-color: lightgray; */\n  display: flex;\n  flex-direction: column;\n  gap: 1rem;\n}\n\n.card {\n  border: 1px solid gray;\n  display: flex;\n  background-color: white;\n  flex-direction: column;\n  padding: 1rem;\n  border-radius: 10px;\n  min-width: 400px;\n}\n\n.cardHeader {\n  display: flex;\n  justify-content: space-between;\n  border-bottom: 1px solid gray;\n  padding-bottom: 8px;\n}\n\n.cardWeatherConditions {\n  display: flex;\n  padding: 8px 0;\n  gap: 1rem;\n}\n\n.cardWeatherImportant {\n  display: flex;\n  align-items: center;\n  gap: .5rem;\n\n}\n\n.cardWeatherConditionsLeft,\n.cardWeatherConditionsRight {\n  flex: 1;\n}\n\n.rc {\n  width: 50%;\n}\n\n.current-real-feel-div,\n.current-wind-div {\n  display: flex;\n  justify-content: space-between;\n}\n\n.current-weather-logo {\n  width: 62px;\n  height: 62px;\n  /* background-color: blue; */\n}\n\n.cardWeatherDetails {\n  display: flex;\n  gap: 1rem;\n}\n\n.cardWeatherDetailsLeft,\n.cardWeatherDetailsRight {\n  flex: 1;\n}\n\n.cardWeatherDetailsLeft>div,\n.cardWeatherDetailsRight>div {\n  display: flex;\n  border-bottom: 1px solid gray;\n  padding: 8px 0;\n}\n\n.cardWeatherDetailsLeft>div>p,\n.cardWeatherDetailsRight>div>p {\n  flex: 1;\n}\n\n.cardWeatherDetailsLeft>div>p:nth-child(2),\n.cardWeatherDetailsRight>div>p:nth-child(2) {\n  text-align: right;\n}\n\n.current-temp-label {\n  font-size: 1.75rem;\n}"],"sourceRoot":""}]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -618,6 +699,36 @@ function styleTagTransform(css, styleElement) {
 }
 module.exports = styleTagTransform;
 
+/***/ }),
+
+/***/ "./src/error.js":
+/*!**********************!*\
+  !*** ./src/error.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+const showError = function showError(err) {
+  // const contentDiv = document.querySelector('.content');
+  const errorDiv = document.querySelector('.errorDiv');
+  const errorMessage = document.createElement('p');
+  errorMessage.textContent = err;
+  errorMessage.style.textAlign = 'center';
+
+  errorDiv.classList.remove('hide');
+  errorDiv.appendChild(errorMessage);
+  setTimeout(() => {
+    errorDiv.classList.add('hide');
+    errorDiv.replaceChildren();
+  }, 4000);
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (showError);
+
+
 /***/ })
 
 /******/ 	});
@@ -701,6 +812,8 @@ var __webpack_exports__ = {};
   \**********************/
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ "./src/style.css");
+/* harmony import */ var _error__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./error */ "./src/error.js");
+
 
 
 const API_KEY = '89d2133b42d54c8fa95201905232808';
@@ -743,10 +856,14 @@ const weatherController = function weatherController() {
     const forecastEndPoint = '/forecast.json';
     try {
       const response = await fetch(`${url}${forecastEndPoint}?key=${API_KEY}&q=${location}&days=3&aqi=yes&alerts=yes`);
+      if (!response.ok) {
+        throw new Error('Invalid Location');
+      }
       const forecastWeatherData = await response.json();
       return forecastWeatherData;
     } catch (err) {
-      console.log(err);
+      console.log('1:', err);
+      (0,_error__WEBPACK_IMPORTED_MODULE_1__["default"])(err);
     }
   };
 
@@ -762,10 +879,57 @@ const screenController = async function screenController() {
   const searchForm = document.querySelector('#locationForm');
   const searchTextBox = document.querySelector('.searchBox');
   const tempUnitsBtn = document.querySelector('.tempUnits');
+  let tempUnit = tempUnitsBtn.dataset.temp;
+  const contentDiv = document.querySelector('.content');
+  const cardTemplate = [...contentDiv.children];
 
   // FUNCTION EXPRESSIONS
   const displayWeatherData = function displayWeatherData(weather) {
-    console.log(weather);
+    console.log('displaying weather', weather);
+  };
+  const extractTime = function extractTime(time) {
+    const options = {
+      weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric',
+    };
+    return new Date(time).toLocaleDateString('en-US', options);
+  };
+  function getIconSource(imgLink) {
+    const imgArr = imgLink.split('');
+    return (`http://${imgArr.slice(2).join('')}`);
+  }
+  const updateCurrentWeatherCard = function updateCurrentWeatherCard(currentWeather) {
+    const currentCard = document.querySelector('.currentCard');
+    const currentTempLabel = document.querySelector('.current-temp-label');
+    const currentCity = document.querySelector('.current-city-label');
+    const currentConditions = document.querySelector('.current-conditions-label');
+    const currentRealFeel = document.querySelector('.current-realfeel-label');
+    const currentTimeLabel = document.querySelector('.current-time-label');
+    const currentWeatherLogo = document.querySelector('#weatherLogo');
+    const currentWind = document.querySelector('.current-wind-label');
+
+    contentDiv.replaceChildren(); // Remove the current card's contents
+    contentDiv.appendChild(cardTemplate[0]); // appends the default card template to page, read to be overwritten
+    currentCard.classList.remove('hide');
+    currentTempLabel.textContent = `${currentWeather.current[`temp_${tempUnit}`]}°${tempUnit.toUpperCase()}`;
+    currentCity.textContent = `Current - ${currentWeather.location.name}, ${currentWeather.location.region}`;
+    currentConditions.textContent = `${currentWeather.current.condition.text}`;
+    currentRealFeel.textContent = `${currentWeather.current[`feelslike_${tempUnit}`]}°${tempUnit.toUpperCase()}`;
+    currentTimeLabel.textContent = extractTime(currentWeather.current.last_updated);
+    currentWind.textContent = `${currentWeather.current.wind_dir} ${currentWeather.current.wind_mph} mph`;
+    currentWeatherLogo.src = getIconSource(currentWeather.current.condition.icon);
+  };
+  const toggleTempUnit = function toggleTempUnit() {
+    switch (tempUnitsBtn.dataset.temp) {
+      case 'f':
+        tempUnitsBtn.dataset.temp = 'c';
+        tempUnitsBtn.textContent = '°C';
+        break;
+      case 'c':
+        tempUnitsBtn.dataset.temp = 'f';
+        tempUnitsBtn.textContent = '°F';
+        break;
+    }
+    tempUnit = tempUnitsBtn.dataset.temp;
   };
   const searchBtnHandler = function searchBtnHandler() {
     if (searchTextBox.validity.valueMissing) {
@@ -781,15 +945,20 @@ const screenController = async function screenController() {
     console.log('Getting weather information..');
     try {
       const forecastData = await weatherControl.getForecastWeather(userLocation);
+      if (!forecastData) {
+        return;
+      }
       displayWeatherData(forecastData);
+      updateCurrentWeatherCard(forecastData);
     } catch (err) {
-      console.log(err);
+      console.log('Error2', err);
     }
   };
 
   // DOM EVENT LISTENERS
   locationSearchBtn.addEventListener('click', searchBtnHandler);
   searchForm.addEventListener('submit', searchFormHandler);
+  tempUnitsBtn.addEventListener('click', toggleTempUnit);
 };
 
 screenController();
